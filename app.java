2ab677bf85ac012311bb9d6dbdbf87e75861b654
The Nhat <nhat0103st@gmail.com>
2021-01-02 22:34:02 +0700

    add two file to repo Initial commit

---
+++ app.java
@@ -0,0 +1,17 @@
+import java.until.*;
+import java.lang.*;
+
+public class app {
+   private String name;
+   private String version;
+   
+   public app(String name, String version){
+   	this.name = name;
+   	this.version = version;
+   }
+
+   public void doSomeThings(){
+   	//do some thing
+   }
+
+}
